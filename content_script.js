c4c05b267daf1cc510630e7ef7d62581b7aa685b
Ramon Brito <ramon.dsb@gmail.com>
2017-09-06 17:03:34 -0400

    Add styled mark test

--- content_script.js
+++ content_script.js
@@ -4,8 +4,15 @@ function isRickURL(url) {
 }
 
 function markElement(element) {
-    // TODO: Experimental modification of element
-    element.innerHTML = "Test";
+    // Create span element
+    var span = document.createElement('span');
+    var spanStyle = 'border-radius: 10px; background-color: red;color: white;text-align: center;position: absolute;font-size: 10px; width: 15px;height: 15px;line-height:15px';
+    span.setAttribute('style', spanStyle);
+    var spanText = document.createTextNode('!');
+    span.appendChild(spanText);
+
+    // Append span to a link
+    element.appendChild(span);
 }
 
 function markLinks() {
@@ -17,7 +24,6 @@ function markLinks() {
     for (var link of links) {
         if (isRickURL(link.href)) {
             // Mark element
-            console.log(link);
             markElement(link);
         }
     }
